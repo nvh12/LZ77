848a11f27da54f07724abf3cb7e14100f193f9a2
nvh12 <154247185+nvh12@users.noreply.github.com>
2025-01-05 11:59:39 +0700

    Added the compare part, removed exe and jar files

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.sql.Time;
 import java.util.*;
 
 public class Main {
@@ -15,24 +16,65 @@ public class Main {
         System.out.println("1. Linear Scan");
         System.out.println("2. Hash Map Search");
         System.out.println("3. Substring Search");
+        System.out.println("4. Test and compare");
         int option = scanner.nextInt();
         List<lz77Set> res;
+        long start, end;
         switch (option) {
             case 1:
+                start = System.nanoTime();
                 res = lz77.linearCompress(str, 16, 16);
+                end = System.nanoTime();
+                System.out.println("Runtime: " + (end - start) + "ns");
+                for (lz77Set r : res) {
+                    System.out.print(r.toString() + " ");
+                }
                 break;
             case 2:
+                start = System.nanoTime();
                 res = lz77.hashMapCompress(str, 16, 16);
+                end = System.nanoTime();
+                System.out.println("Runtime: " + (end - start) + "ns");
+                for (lz77Set r : res) {
+                    System.out.print(r.toString() + " ");
+                }
                 break;
             case 3:
+                start = System.nanoTime();
                 res = lz77.substringSearchCompress(str, 16, 16);
+                end = System.nanoTime();
+                System.out.println("Runtime: " + (end - start) + "ns");
+                for (lz77Set r : res) {
+                    System.out.print(r.toString() + " ");
+                }
+                break;
+            case 4:
+                start = System.nanoTime();
+                res = lz77.linearCompress(str, 16, 16);
+                end = System.nanoTime();
+                System.out.println("Runtime: " + (end - start) + "ns");
+                for (lz77Set r : res) {
+                    System.out.print(r.toString() + " ");
+                }
+                System.out.println();
+                start = System.nanoTime();
+                res = lz77.hashMapCompress(str, 16, 16);
+                end = System.nanoTime();
+                System.out.println("Runtime: " + (end - start) + "ns");
+                for (lz77Set r : res) {
+                    System.out.print(r.toString() + " ");
+                }
+                System.out.println();
+                start = System.nanoTime();
+                res = lz77.substringSearchCompress(str, 16, 16);
+                end = System.nanoTime();
+                System.out.println("Runtime: " + (end - start) + "ns");
+                for (lz77Set r : res) {
+                    System.out.print(r.toString() + " ");
+                }
                 break;
             default:
-                res = new ArrayList<>();
                 break;
-        }
-        for (lz77Set r : res) {
-            System.out.print(r.toString() + " ");
         }
         scanner.nextLine();
         scanner.nextLine();
